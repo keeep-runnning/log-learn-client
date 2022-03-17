eb01bb3a363e68d3ff0a9ff74705e4c4e5e9587b
jm kim <rookie2000g@gmail.com>
2022-03-17 14:46:54 +0900

    Chore: configure msw

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+if (process.env.NODE_ENV === 'development') {
+  import("./mocks/browser").then(({ worker }) => worker.start());
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
