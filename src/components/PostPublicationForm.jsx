86646efecb9819ddd602479a6701ec9ecfb05fde
jm kim <rookie2000g@gmail.com>
2022-03-24 16:37:14 +0900

    Feat(PostPublicationForm): call api to publish blog post

--- src/components/PostPublicationForm.jsx
+++ src/components/PostPublicationForm.jsx
@@ -3,8 +3,25 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import { useCallback, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { Editor } from "@toast-ui/react-editor";
+import { useMutation, useQuery } from "react-query";
+import { useNavigate, Navigate } from "react-router-dom";
+import axios from "axios";
+
+async function publishPost({ title, content }) {
+  const response = await axios.post("/api/posts", {title, content});
+  return response.data;
+}
+
+function getPostTitleUrlFromPostTitle(postTitle="") {
+  const specialCharacter = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
+  const whitespace = /[\s]+/g;
+  return postTitle.trim()
+    .replaceAll(specialCharacter, " ")
+    .replaceAll(whitespace, "-");
+}
 
 const PostPublicationForm = () => {
+  const navigate = useNavigate();
   const editorRef = useRef();
   const { register, handleSubmit, setFocus } = useForm();
 
@@ -12,17 +29,34 @@ const PostPublicationForm = () => {
     setFocus("title");
   }, [setFocus]);
 
+  const postPublicationMutation = useMutation(publishPost, {
+    onSuccess: ({id: postId, title, author}) => {
+      const urlTitle = getPostTitleUrlFromPostTitle(title);
+      navigate(`/@${author}/${postId}/${urlTitle}`);
+    }
+  });
+
   const handlePostPublication = useCallback(handleSubmit((data) => {
     const { title } = data;
     const content = editorRef.current.getInstance().getMarkdown();
+    postPublicationMutation.mutate({title, content});
   }), []);
+
+  const { data: currentUser, isSuccess } = useQuery("currentUser", async () => {
+    const response = await axios.get("/api/currentUser");
+    return response.data;
+  });
+
+  if(isSuccess && !currentUser.isLoggedIn) {
+    return <Navigate to="/login" replace={true} />;
+  }
 
   return (
     <form onSubmit={handlePostPublication}
           className="h-full md:w-11/12 md:mx-auto flex flex-col gap-y-4 px-2 py-4"
     >
       <div className="flex justify-end">
-        <button type="submit" className="btn btn-indigo">
+        <button disabled={postPublicationMutation.isLoading} type="submit" className="btn btn-indigo">
           글 발행하기
         </button>
       </div>
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,18 +6,25 @@ const DELAY_TIME_MS = 800;
 let id = 0;
 
 const db = factory({
-  user: {
+  users: {
     id: primaryKey(() => String(id++)),
     username: String,
     email: String,
     password: String,
+  },
+  posts: {
+    id: primaryKey(() => String(id++)),
+    title: String,
+    content: String,
+    author: String,
+    createdAt: String,
   }
 });
 
 export const handlers = [
   rest.post("/api/users", (req, res, ctx) => {
     const { username, email, password } = req.body;
-    const createdUser = db.user.create({username, email, password});
+    const createdUser = db.users.create({username, email, password});
 
     return res(
       ctx.status(201),
@@ -27,7 +34,7 @@ export const handlers = [
   }),
   rest.post("/api/login", (req, res, ctx) => {
     const { email, password } = req.body;
-    const user = db.user.findFirst({
+    const user = db.users.findFirst({
       where: {
         email: {
           equals: email
@@ -79,6 +86,33 @@ export const handlers = [
       ctx.json({
         isLoggedIn: false
       })
+    );
+  }),
+  rest.post("/api/posts", (req, res, ctx) => {
+    const currentUsername = localStorage.getItem("[mockData]currentUsername");
+    if(!currentUsername) {
+      return res(
+        ctx.status(401),
+        ctx.delay(DELAY_TIME_MS),
+        ctx.json({
+          code: "common-002",
+          errorMessage: "로그인이 필요합니다.",
+          errors: []
+        })
+      );
+    }
+    const { title, content } = req.body;
+    const createdPost = db.posts.create({
+      title,
+      content,
+      author: currentUsername,
+      createdAt: new Date().toISOString()
+    });
+
+    return res(
+      ctx.status(201),
+      ctx.delay(DELAY_TIME_MS),
+      ctx.json(createdPost)
     );
   })
 ];
