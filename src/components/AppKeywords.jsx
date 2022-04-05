e36bb10deb4c0d545239c46ff80b883149ba3ea9
jm kim <rookie2000g@gmail.com>
2022-04-05 16:00:28 +0900

    Design(Home): remove tailwindcss and use emotion

--- src/components/AppKeywords.jsx
+++ src/components/AppKeywords.jsx
@@ -1,15 +1,76 @@
+import styled from "@emotion/styled";
+import { css, keyframes } from "@emotion/react";
+
+const appKeywords = [
+  {
+    keyword: "Simple",
+    textGradientCss: css`
+      background: linear-gradient(90deg, rgba(237,255,0,1) 33%, rgba(255,154,0,1) 66%);
+      background-clip: text;
+    `
+  },
+  {
+    keyword: "Blog Service",
+    textGradientCss: css`
+      background: linear-gradient(90deg, rgba(0,255,171,1) 33%, rgba(0,112,255,1) 66%);
+      background-clip: text;
+    `
+  },
+  {
+    keyword: "For Developers",
+    textGradientCss: css`
+      background: linear-gradient(90deg, rgba(237,96,213,1) 33%, rgba(99,102,241,1) 66%);
+      background-clip: text;
+    `
+  }
+];
+const NUM_OF_KEYWORD = appKeywords.length;
+const ACTIVE_TIME_IN_SECOND = 4;
+const ACTIVE_TIME_IN_PERCENT = 100 / NUM_OF_KEYWORD;
+
+const activateKeyword = keyframes`
+  0%, ${ACTIVE_TIME_IN_PERCENT}%, 100% {
+    color: black;
+  }
+  ${ACTIVE_TIME_IN_PERCENT / 10}%, ${ACTIVE_TIME_IN_PERCENT - (ACTIVE_TIME_IN_PERCENT / 10)}% {
+    color: transparent;
+  }
+`;
+
+const AppKeyword = styled.span`
+  animation-name: ${activateKeyword};
+  animation-duration: ${NUM_OF_KEYWORD * ACTIVE_TIME_IN_SECOND}s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+  animation-delay: ${({order}) => parseInt(order) * ACTIVE_TIME_IN_SECOND}s;
+`;
+
 const AppKeywords = () => {
   return (
-    <section className="text-center py-14 flex flex-col gap-y-14 md:gap-y-16 items-center">
-      <div className="app-keyword from-yellow-500 to-orange-500 animate-[activate-text_9s_ease_infinite]">
-        Simple
-      </div>
-      <div className="app-keyword from-green-500 to-blue-500 animate-[activate-text_9s_ease_3s_infinite]">
-        Blog Service
-      </div>
-      <div className="app-keyword from-pink-500 to-indigo-500 animate-[activate-text_9s_ease_6s_infinite]">
-        For Developers
-      </div>
+    <section
+      css={theme => css`
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        row-gap: 40px;
+        font-size: 40px;
+        line-height: 60px;
+        color: black;
+        font-weight: ${theme.textWeight.bold};
+        
+        ${theme.mq.md} {
+          padding-top: 56px;
+          padding-bottom: 56px;
+          row-gap: 56px;
+          font-size: 56px;
+          line-height: 80px;
+        }
+      `}>
+      {appKeywords.map(({ keyword, textGradientCss }, idx) =>
+        (<AppKeyword key={idx} order={idx} css={textGradientCss}>{keyword}</AppKeyword>)
+      )}
     </section>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,25 @@
-import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
 
 import AppKeywords from "../components/AppKeywords";
+import AppIntroduction from "../components/AppIntroduction";
 
 const Home = () => {
   return (
-    <>
-      <div className="my-4 md:container md:mx-auto space-y-4 md:space-y-8">
-        <AppKeywords />
-        <section className="max-w-screen-md mx-4 md:mx-auto rounded p-6 text-center
-          bg-indigo-500 md:bg-white text-white md:text-black space-y-4">
-          <p className="text-lg">
-            쉽고 간단한 블로그 서비스, log learn 을 이용해 학습한 내용을 정리해 보세요
-          </p>
-          <Link to="/signup"
-            className="inline-block p-2 bg-indigo-700 md:bg-white md:text-black
-              md:border-2 md:border-indigo-500 rounded hover:scale-105 duration-300"
-          >
-            시작하기
-          </Link>
-        </section>
-      </div>
-    </>
+    <div
+      css={theme => css`
+        padding: ${theme.spacing[4]};
+        & > * + * {
+          margin-top: ${theme.spacing[4]};
+        }
+        ${theme.mq.md} {
+          & > * + * {
+            margin-top: ${theme.spacing[8]};
+          }
+        }
+    `}>
+      <AppKeywords />
+      <AppIntroduction />
+    </div>
   );
 };
 
