fe7d06b8312c6a1308eb8669dc7543888142b4bd
jm kim <rookie2000g@gmail.com>
2022-05-03 22:52:47 +0900

    Feat(LogoutButton): display toast notification on logout

--- src/components/user/LogoutButton.jsx
+++ src/components/user/LogoutButton.jsx
@@ -3,13 +3,22 @@ import { useMutation, useQueryClient } from "react-query";
 import PropTypes from "prop-types";
 
 import { logout } from "../../apis";
+import useNotifications from "../../hooks/useNotifications";
+import { NOTIFICATION_TYPE } from "../../constants/notifications";
 
 const LogoutButton = ({ children }) => {
   const queryClient = useQueryClient();
 
+  const { addNotification } = useNotifications();
+
   const logoutMutation = useMutation(logout, {
     onSuccess: () => {
       queryClient.invalidateQueries("currentUser");
+      addNotification({
+        type: NOTIFICATION_TYPE.SUCCESS,
+        content: "로그아웃 되었습니다.",
+        isAutoClose: true
+      });
     }
   });
 
