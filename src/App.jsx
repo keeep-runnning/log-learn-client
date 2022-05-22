e007ce17677c3b3a8b012818a67e3c433409e24a
jm kim <rookie2000g@gmail.com>
2022-05-22 21:52:04 +0900

    Feat: add NotFound page component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Login from "./pages/Login";
 import UserHome from "./pages/UserHome";
 import PostPublication from "./pages/PostPublication";
 import PostDetail from "./pages/PostDetail";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   const setCurrentUser = useSetRecoilState(currentUserState);
@@ -31,16 +32,17 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="signup" element={<SignUp/>}/>
-        <Route path="login" element={<Login/>}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
       </Route>
-      <Route path="/@:username" element={<Layout/>}>
-        <Route index element={<UserHome/>}/>
-        <Route path="posts/:postId" element={<PostDetail/>}/>
+      <Route path="/@:username" element={<Layout />}>
+        <Route index element={<UserHome />} />
+        <Route path="posts/:postId" element={<PostDetail />} />
       </Route>
-      <Route path="/write" element={<PostPublication/>}/>
+      <Route path="/write" element={<PostPublication />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,62 @@
+import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
+
+import Logo from "../components/common/Logo";
+import PrimaryButton from "../components/common/buttons/PrimaryButton";
+
+const NotFound = () => {
+  return (
+    <div css={theme => css`
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background-color: ${theme.bgColor[2]};
+    `}>
+      <div css={theme => css`
+        padding: ${theme.spacing[8]};
+        max-width: ${theme.bp.md};
+        margin: 96px auto 0;
+        display: flex;
+        flex-direction: column;
+        row-gap: 96px;
+      `}>
+        <header css={theme => css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          row-gap: ${theme.spacing[8]};
+        `}>
+          <Link to="/">
+            <Logo />
+          </Link>
+          <span css={theme => css`
+            font-size: 48px;
+            font-weight: ${theme.textWeight.bold};
+            color: ${theme.primaryColor[3]};
+          `}>404</span>
+        </header>
+        <section css={theme => css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          row-gap: ${theme.spacing[8]};
+        `}>
+          <h1 css={theme => css`
+            font-weight: ${theme.textWeight.bold};
+            ${theme.textSize["2xl"]}
+          `}>페이지를 찾지 못했습니다.</h1>
+          <p css={theme => css`
+            ${theme.textSize.lg}
+          `}>잘못된 주소이거나, 더 이상 제공하지 않는 페이지입니다.</p>
+          <PrimaryButton as={Link} to="/">
+            메인 페이지로 이동
+          </PrimaryButton>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
