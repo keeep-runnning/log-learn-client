327d19966ca6fe6dda3d924f9aa2b89338b4327e
jm kim <rookie2000g@gmail.com>
2022-05-25 16:06:16 +0900

    Refactor: define and use usePostRemoval custom hook

--- src/components/post/PostRemoveButton.jsx
+++ src/components/post/PostRemoveButton.jsx
@@ -1,34 +1,30 @@
 import { memo, useCallback } from "react";
-import { useMutation, useQueryClient } from "react-query";
 import { useNavigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import DefaultButton from "../common/buttons/DefaultButton";
-import { removePost } from "../../apis";
 import useNotifications from "../../hooks/useNotifications";
 import { NOTIFICATION_TYPE } from "../../constants/notifications";
+import usePostRemoval from "../../hooks/queries/posts/usePostRemoval";
 
 const PostRemoveButton = ({ post }) => {
-  const queryClient = useQueryClient();
-
   const navigate = useNavigate();
 
   const { addNotification } = useNotifications();
 
-  const postRemoveMutation = useMutation(postId => removePost(postId), {
-    onSuccess: () => {
-      addNotification({
-        content: `블로그 포스트[${post.title}]가 삭제되었습니다.`,
-        type: NOTIFICATION_TYPE.SUCCESS,
-        isAutoClose: true
-      });
-      queryClient.invalidateQueries([post.author, "posts"]);
-      navigate(`/@${post.author}`, { replace: true });
-    }
-  });
+  const postRemoveMutation = usePostRemoval();
 
   const handlePostRemoveButtonClick = useCallback(() => {
-    postRemoveMutation.mutate(post.id);
+    postRemoveMutation.mutate(post, {
+      onSuccess: () => {
+        addNotification({
+          content: `블로그 포스트[${post.title}]가 삭제되었습니다.`,
+          type: NOTIFICATION_TYPE.SUCCESS,
+          isAutoClose: true
+        });
+        navigate(`/@${post.author}`, { replace: true });
+      }
+    });
   }, [post]);
 
   return (
---
+++ src/hooks/queries/posts/usePostRemoval.js
@@ -0,0 +1,23 @@
+import { useMutation, useQueryClient } from "react-query";
+
+import apiClient from "../apiClient";
+import { postKeys } from "./queryKeys";
+
+const removePost = async (postId) => {
+  const response = await apiClient.delete(`/api/posts/${postId}`);
+  return response.data;
+};
+
+const usePostRemoval = () => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    post => removePost(post.id),
+    {
+      onSuccess: (data, { author }) => {
+        queryClient.invalidateQueries(postKeys.list(author));
+      }
+    }
+  );
+};
+
+export default usePostRemoval;
