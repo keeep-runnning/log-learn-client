ced1d6e5649943cb3970f695362eefda6d86013d
jm kim <rookie2000g@gmail.com>
2022-05-27 14:52:07 +0900

    Refactor: remove apis directory

--- src/apis/users.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function signUp({ username, email, password }) {
-  const response = await axios.post("/api/users", { username, email, password });
-  return response.data;
-}
