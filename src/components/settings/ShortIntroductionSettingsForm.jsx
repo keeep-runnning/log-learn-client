1886a7053156d265b7e1e9fd11e1bf4dc1c4f674
jmkim <rookie2000g@gmail.com>
2022-06-10 17:51:03 +0900

    Feat(ShortIntroductionSettingsForm): display toast notification when short introduction is changed successfully

--- src/components/settings/ShortIntroductionSettingsForm.jsx
+++ src/components/settings/ShortIntroductionSettingsForm.jsx
@@ -5,6 +5,7 @@ import { useCallback } from "react";
 import RowForm from "./RowForm";
 import PrimaryButton from "../common/buttons/PrimaryButton";
 import useShortIntroductionSettings from "../../hooks/queries/settings/useShortIntroductionSettings";
+import useNotifications from "../../hooks/useNotifications";
 
 const SHORT_INTRODUCTION_FIELD_NAME = "shortIntroduction";
 
@@ -17,8 +18,14 @@ const ShortIntroductionSettingsForm = ({ data }) => {
 
   const shortIntroductionSettingsMutation = useShortIntroductionSettings();
 
+  const { notifySuccess } = useNotifications();
+
   const onValid = useCallback(({ shortIntroduction }) => {
-    shortIntroductionSettingsMutation.mutate(shortIntroduction);
+    shortIntroductionSettingsMutation.mutate(shortIntroduction, {
+      onSuccess: () => {
+        notifySuccess({ content: "짧은 소개가 수정되었습니다." });
+      }
+    });
   }, []);
 
   return (
