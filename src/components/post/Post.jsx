949741dae2d5c178aff49fe489f85efd67907101
jmkim <rookie2000g@gmail.com>
2022-11-13 20:19:05 +0900

    Design(Post): use chakra-ui for styling

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,169 +1,116 @@
 import "@toast-ui/editor/dist/toastui-editor-viewer.css";
-
-import { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
-import { css, keyframes } from "@emotion/react";
+import { useCallback, useEffect, useRef } from "react";
 import { Viewer } from "@toast-ui/react-editor";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-
-import DefaultButton from "../common/buttons/DefaultButton";
+import { Link as ReactRouterLink, useNavigate } from "react-router-dom";
 import PostEditForm from "./PostEditForm";
 import useCurrentUserQuery from "../../hooks/queries/auth/useCurrentUserQuery";
-import PostRemoveButton from "./PostRemoveButton";
 import DateTime from "../common/DateTime";
 import pageUrl from "../../utils/pageUrl";
+import {
+  Button,
+  ButtonGroup,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  Link,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
+import usePostRemoval from "../../hooks/queries/posts/usePostRemoval";
 
-const SLIDE_ANIMATION_TIME_IN_MS = 200;
-
-const slideUpAnimation = keyframes`
-  from {
-    transform: translateY(100%);
-  }
-  to {
-    transform: translateY(0%);
-  }
-`;
-
-const Post = ({ post }) => {
+export default function Post({ post }) {
   const viewerRef = useRef();
-  const currentUser = useCurrentUserQuery();
-  const [isEditFormDisplayed, setIsEditFormDisplayed] = useState(false);
-  const [isEditFormClosing, setIsEditFormClosing] = useState(false);
 
-  useEffect(() => {
-    viewerRef.current.getInstance().setMarkdown(post.content);
+  const currentUser = useCurrentUserQuery();
+
+  const {
+    isOpen: isEditFormOpen,
+    onOpen: onOpenEditForm,
+    onClose: onCloseEditForm,
+  } = useDisclosure();
+
+  const postRemoveMutation = usePostRemoval();
+
+  const navigate = useNavigate();
+
+  const toast = useToast();
+
+  const handlePostRemoveButtonClick = useCallback(() => {
+    postRemoveMutation.mutate(post, {
+      onSuccess: () => {
+        toast({
+          description: `[${post.title}] 블로그 포스트가 삭제되었습니다`,
+          position: "top",
+          status: "success",
+          isClosable: true,
+        });
+        navigate(pageUrl.getUserHomePageUrl(post.author), { replace: true });
+      },
+    });
   }, [post]);
 
   useEffect(() => {
-    if (isEditFormClosing) {
-      const timerId = setTimeout(() => {
-        setIsEditFormDisplayed(false);
-        setIsEditFormClosing(false);
-      }, SLIDE_ANIMATION_TIME_IN_MS);
+    viewerRef.current.getInstance().setMarkdown(post.content);
+  }, [post.content]);
 
-      return () => {
-        clearTimeout(timerId);
-      };
-    }
-  }, [isEditFormClosing]);
-
-  useLayoutEffect(() => {
-    if (isEditFormDisplayed) {
-      document.body.style.overflowY = "hidden";
-
-      return () => {
-        document.body.style.removeProperty("overflow-y");
-      };
-    }
-  }, [isEditFormDisplayed]);
-
-  const displayEditForm = useCallback(() => {
-    setIsEditFormDisplayed(true);
-  }, []);
-
-  const closeEditForm = useCallback(() => {
-    setIsEditFormClosing(true);
-  }, []);
-
-  const postData = useMemo(
-    () => ({
-      id: post.id,
-      title: post.title,
-      content: post.content,
-    }),
-    [post]
-  );
+  const postToEdit = {
+    id: post.id,
+    title: post.title,
+    content: post.content,
+  };
 
   return (
     <>
-      <article
-        css={(theme) => css`
-          display: flex;
-          flex-direction: column;
-          row-gap: ${theme.spacing[6]};
-        `}
-      >
-        <header
-          css={(theme) => css`
-            display: flex;
-            flex-direction: column;
-            row-gap: ${theme.spacing[4]};
-            h1 {
-              font-weight: ${theme.textWeight.bold};
-              ${theme.textSize["2xl"]}
-            }
-          `}
-        >
-          <h1>{post.title}</h1>
-          <div
-            css={(theme) => css`
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-              ${theme.textSize.sm}
-            `}
-          >
-            <div
-              css={(theme) => css`
-                display: flex;
-                align-items: center;
-                column-gap: ${theme.spacing[2]};
-              `}
-            >
+      <Flex as="article" direction="column" rowGap={6}>
+        <Flex as="header" direction="column" rowGap={4}>
+          <Heading as="h1" fontSize="2xl" fontWeight="bold">
+            {post.title}
+          </Heading>
+          <Flex alignItems="center" justifyContent="space-between">
+            <Flex columnGap={2} alignItems="center">
               <Link
+                as={ReactRouterLink}
                 to={pageUrl.getUserHomePageUrl(post.author)}
-                css={(theme) => css`
-                  ${theme.textSize.sm}
-                  font-weight: ${theme.textWeight.bold};
-                  &:hover {
-                    text-decoration: underline;
-                  }
-                `}
+                fontWeight="bold"
               >
                 {post.author}
               </Link>
               &middot;
               <DateTime dateTimeStr={post.createdAt} />
-            </div>
+            </Flex>
             {currentUser.isLoggedIn && currentUser.username === post.author && (
-              <div
-                css={(theme) => css`
-                  display: flex;
-                  column-gap: ${theme.spacing[2]};
-                `}
-              >
-                <DefaultButton onClick={displayEditForm}>수정</DefaultButton>
-                <PostRemoveButton post={post} />
-              </div>
+              <ButtonGroup size="sm" variant="ghost">
+                <Button onClick={onOpenEditForm}>수정</Button>
+                <Button
+                  colorScheme="red"
+                  isDisabled={postRemoveMutation.isLoading}
+                  isLoading={postRemoveMutation.isLoading}
+                  loadingText="삭제 중..."
+                  onClick={handlePostRemoveButtonClick}
+                >
+                  삭제
+                </Button>
+              </ButtonGroup>
             )}
-          </div>
-        </header>
+          </Flex>
+        </Flex>
         <Viewer ref={viewerRef} initialValue={post.content} usageStatistics={false} />
-      </article>
-
-      {isEditFormDisplayed && (
-        <section
-          css={(theme) => css`
-            background-color: ${theme.bgColor[2]};
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            animation: ${slideUpAnimation} ${SLIDE_ANIMATION_TIME_IN_MS}ms ease-in;
-            transition: transform ${SLIDE_ANIMATION_TIME_IN_MS}ms ease-in;
-            ${isEditFormClosing &&
-            css`
-              transform: translateY(100%);
-            `}
-          `}
-        >
-          <PostEditForm postData={postData} onClose={closeEditForm} />
-        </section>
-      )}
+      </Flex>
+      <Drawer isOpen={isEditFormOpen} onClose={onCloseEditForm} placement="bottom" size="full">
+        <DrawerOverlay />
+        <DrawerContent bgColor="gray.50">
+          <DrawerBody>
+            <PostEditForm postData={postToEdit} onClose={onCloseEditForm} />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
-};
+}
 
 Post.propTypes = {
   post: PropTypes.shape({
@@ -174,5 +121,3 @@ Post.propTypes = {
     content: PropTypes.string,
   }).isRequired,
 };
-
-export default Post;
