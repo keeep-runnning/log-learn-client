1d69ffad301692ef14dc74f5ea4a28fa3f096965
jmkim <rookie2000g@gmail.com>
2022-11-15 16:10:41 +0900

    Design(UsernameSettingsForm): use chakra-ui for styling

--- src/components/settings/UsernameSettingsForm.jsx
+++ src/components/settings/UsernameSettingsForm.jsx
@@ -1,17 +1,20 @@
 import { useCallback, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useForm } from "react-hook-form";
-
-import PrimaryButton from "../common/buttons/PrimaryButton";
-import RowForm from "./RowForm";
 import useUsernameSettings from "../../hooks/queries/settings/useUsernameSettings";
-import FormFieldErrorMessage from "../common/FormFieldErrorMessage";
-import useNotifications from "../../hooks/useNotifications";
-import { usernameValidation } from "../../utils/formValidation";
-import { usernameValidationErrorMessage } from "../../utils/formValidationErrorMessage";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
 
-const UsernameSettingsForm = ({ data }) => {
-  const { notifySuccess } = useNotifications();
+export default function UsernameSettingsForm({ data }) {
+  const toast = useToast();
 
   const {
     register,
@@ -32,16 +35,21 @@ const UsernameSettingsForm = ({ data }) => {
 
   const usernameSettingsMutation = useUsernameSettings();
 
-  const onValid = useCallback(({ username }) => {
+  const setUsername = useCallback(({ username }) => {
     usernameSettingsMutation.mutate(username, {
       onSuccess: () => {
-        notifySuccess({ content: "유저이름이 변경되었습니다." });
+        toast({
+          description: "유저이름이 변경되었습니다",
+          position: "top",
+          status: "success",
+          isClosable: true,
+        });
       },
       onError: (error) => {
         if (error.response) {
           const { code } = error.response.data;
           if (code === "user-001") {
-            setError("username", { type: "unique", message: "이미 사용중인 유저이름 입니다." });
+            setError("username", { type: "unique", message: "이미 사용 중인 유저이름 입니다" });
           }
         }
       },
@@ -49,46 +57,63 @@ const UsernameSettingsForm = ({ data }) => {
   }, []);
 
   return (
-    <RowForm onSubmit={handleSubmit(onValid)}>
-      <label htmlFor="username">유저이름</label>
-      <section>
-        <input
-          id="username"
-          type="text"
-          {...register("username", {
-            required: usernameValidationErrorMessage.required,
-            minLength: {
-              value: usernameValidation.minLength,
-              message: usernameValidationErrorMessage.length,
-            },
-            maxLength: {
-              value: usernameValidation.maxLength,
-              message: usernameValidationErrorMessage.length,
-            },
-            pattern: {
-              value: usernameValidation.pattern,
-              message: usernameValidationErrorMessage.pattern,
-            },
-            validate: {
-              isChanged: (newUsername) =>
-                usernameValidation.isChanged({
-                  oldUsername: data,
-                  newUsername,
-                }) || usernameValidationErrorMessage.isChanged,
-            },
-          })}
-        />
-        <FormFieldErrorMessage message={errors.username?.message} />
-        <PrimaryButton type="submit" disabled={usernameSettingsMutation.isLoading}>
-          수정하기
-        </PrimaryButton>
-      </section>
-    </RowForm>
+    <Flex
+      as="form"
+      onSubmit={handleSubmit(setUsername)}
+      direction="column"
+      alignItems="flex-end"
+      rowGap={4}
+    >
+      <FormControl isInvalid={Boolean(errors.username)}>
+        <Flex direction={{ base: "column", md: "row" }}>
+          <FormLabel htmlFor="username" width={{ md: "25%" }}>
+            유저이름
+          </FormLabel>
+          <Box flexGrow={{ md: 1 }}>
+            <Input
+              id="username"
+              type="text"
+              {...register("username", {
+                required: "유저이름을 입력해주세요",
+                minLength: {
+                  value: 2,
+                  message: "유저이름을 2자 이상 20자 이하로 입력해주세요",
+                },
+                maxLength: {
+                  value: 20,
+                  message: "유저이름을 2자 이상 20자 이하로 입력해주세요",
+                },
+                pattern: {
+                  value: /^[ㄱ-ㅎ가-힣\w-]+$/,
+                  message:
+                    "한글/영문 대소문자/숫자/언더바(_)/하이픈(-)만을 이용해 유저이름을 입력해주세요",
+                },
+                validate: {
+                  isChanged: (newUsername) =>
+                    data !== newUsername || "입력하신 유저이름이 기존 유저이름과 동일합니다",
+                },
+              })}
+            />
+            {errors.username ? (
+              <FormErrorMessage>{errors.username.message}</FormErrorMessage>
+            ) : null}
+          </Box>
+        </Flex>
+      </FormControl>
+      <Button
+        type="submit"
+        size="sm"
+        colorScheme="main"
+        isDisabled={usernameSettingsMutation.isLoading}
+        isLoading={usernameSettingsMutation.isLoading}
+        loadingText="수정 중..."
+      >
+        수정하기
+      </Button>
+    </Flex>
   );
-};
+}
 
 UsernameSettingsForm.propTypes = {
   data: PropTypes.string.isRequired,
 };
-
-export default UsernameSettingsForm;
