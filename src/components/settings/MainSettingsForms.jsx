2dc46fc599487d4a4691088628dd67aba3d3cb90
jmkim <rookie2000g@gmail.com>
2022-11-15 23:50:07 +0900

    Design(MainSettingsForms): use chakra-ui for styling

--- src/components/settings/MainSettingsForms.jsx
+++ src/components/settings/MainSettingsForms.jsx
@@ -1,38 +1,17 @@
-import { css } from "@emotion/react";
 import { useOutletContext } from "react-router-dom";
-
 import UsernameSettingsForm from "./UsernameSettingsForm";
 import EmailSettingsForm from "./EmailSettingsForm";
 import ShortIntroductionSettingsForm from "./ShortIntroductionSettingsForm";
+import { StackDivider, VStack } from "@chakra-ui/react";
 
-const MainSettingsForms = () => {
+export default function MainSettingsForms() {
   const { settingsData } = useOutletContext();
 
   return (
-    <div
-      css={(theme) => css`
-        display: flex;
-        flex-direction: column;
-        row-gap: ${theme.spacing[8]};
-        & > * + * {
-          position: relative;
-          &::before {
-            content: "";
-            background-color: ${theme.lineColor[3]};
-            width: 100%;
-            height: 0.5px;
-            position: absolute;
-            left: 0;
-            top: -${theme.spacing[4]};
-          }
-        }
-      `}
-    >
+    <VStack alignItems="stretch" spacing={10} divider={<StackDivider borderColor="gray.300" />}>
       <UsernameSettingsForm data={settingsData.username} />
       <EmailSettingsForm data={settingsData.email} />
       <ShortIntroductionSettingsForm data={settingsData.shortIntroduction} />
-    </div>
+    </VStack>
   );
-};
-
-export default MainSettingsForms;
+}
