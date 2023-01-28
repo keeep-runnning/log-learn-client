d83d6dd99d17afa8df037ec6bb5ef0c4aa1c64fb
jmkim <rookie2000g@gmail.com>
2023-01-28 15:36:59 +0900

    Chore: uninstall react-query

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import customizedTheme from "./styles/customizedTheme";
 
-const queryClient = new QueryClient();
+import customizedTheme from "./styles/customizedTheme";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={customizedTheme}>
-          <App />
-        </ChakraProvider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <ChakraProvider theme={customizedTheme}>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
