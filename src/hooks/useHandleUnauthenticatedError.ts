7d2766a4a5dbe0553a9ef53c162a0727d05e39b8
jmkim <rookie2000g@gmail.com>
2023-02-16 13:04:48 +0900

    When unauthenticated error occurs, after logging in, return to previous page

--- src/hooks/useHandleUnauthenticatedError.ts
+++ src/hooks/useHandleUnauthenticatedError.ts
@@ -1,21 +1,30 @@
 import { useToast } from "@chakra-ui/react";
 import { useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import pageUrl from "../utils/pageUrl";
+import { LoggedOutMe } from "./useMeQuery";
 
 export default function useHandleUnauthenticatedError() {
   const toast = useToast();
 
   const navigate = useNavigate();
 
+  const location = useLocation();
+
   const queryClient = useQueryClient();
 
   return () => {
-    queryClient.invalidateQueries({
-      queryKey: ["me"],
+    const loggedOutMe: LoggedOutMe = {
+      isLoggedIn: false,
+    };
+    queryClient.setQueryData(["me"], loggedOutMe);
+
+    navigate(pageUrl.getLoginPageUrl(), {
+      replace: true,
+      state: { from: location },
     });
-    navigate(pageUrl.getLoginPageUrl(), { replace: true });
+
     toast({
       title: "로그인 만료",
       description: "로그인이 만료되었습니다. 다시 로그인해주세요",
