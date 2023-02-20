f665e1b661ee05a2bcc968a787cfbbbf9cdcbc32
jmkim <rookie2000g@gmail.com>
2023-02-20 23:36:28 +0900

    Use Infinite scroll to display blog owner's posts

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -3,26 +3,17 @@ import { Flex, Link, StackDivider, VStack } from "@chakra-ui/react";
 
 import { pagePath } from "../../utils/page";
 import DateTime from "../common/DateTime";
-
-type PostListItem = {
-  id: number;
-  title: string;
-  createdAt: Date;
-  author: {
-    id: number;
-    name: string;
-  };
-};
+import { Post } from "../../hooks/usePostsInfiniteQuery";
 
 type PostListProps = {
-  posts: PostListItem[];
+  posts: Post[];
 };
 
 export default function PostList({ posts }: PostListProps) {
   return (
     <VStack alignItems="stretch" spacing={6} divider={<StackDivider borderColor="gray.300" />}>
       {posts.map((post) => (
-        <Flex as="article" direction="column" alignItems="flex-start" rowGap={4} px={2}>
+        <Flex key={post.id} direction="column" rowGap={4} px={2}>
           <Link
             as={ReactRouterLink}
             to={pagePath.getPostDetail(post.id)}
@@ -31,13 +22,7 @@ export default function PostList({ posts }: PostListProps) {
           >
             {post.title}
           </Link>
-          <Flex alignItems="center" columnGap={2}>
-            <Link as={ReactRouterLink} to={pagePath.getBlog(post.author.name)}>
-              {post.author.name}
-            </Link>
-            &middot;
-            <DateTime dateTime={post.createdAt} />
-          </Flex>
+          <DateTime dateTime={post.createdAt} />
         </Flex>
       ))}
     </VStack>
--- src/pages/blog/BlogOwnerPostList.tsx
+++ src/pages/blog/BlogOwnerPostList.tsx
@@ -1,31 +1,40 @@
-import PostList from "../../components/post/PostList";
-import BaseContainer from "../BaseContainer";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { Box } from "@chakra-ui/react";
 
-const dummyPosts = [
-  {
-    id: 10,
-    title: "post title",
-    createdAt: new Date(),
-    author: {
-      id: 20,
-      name: "post author",
-    },
-  },
-  {
-    id: 11,
-    title: "post title 2",
-    createdAt: new Date(),
-    author: {
-      id: 21,
-      name: "post author 2",
-    },
-  },
-];
+import LoadingMessage from "../../components/common/LoadingMessage";
+import PostList from "../../components/post/PostList";
+import usePostsInfiniteQuery from "../../hooks/usePostsInfiniteQuery";
+import BaseContainer from "../BaseContainer";
+import { useBlogOwner } from "./Blog";
 
 export default function BlogOwnerPostList() {
+  const { blogOwner } = useBlogOwner();
+
+  const postsInfiniteQuery = usePostsInfiniteQuery(blogOwner.username);
+
+  const [fetchTriggerRef, fetchTriggerInView] = useInView();
+
+  useEffect(() => {
+    if (fetchTriggerInView) {
+      postsInfiniteQuery.fetchNextPage();
+    }
+  }, [fetchTriggerInView]);
+
+  if (postsInfiniteQuery.data) {
+    const posts = postsInfiniteQuery.data.pages.flatMap((page) => page.posts);
+
+    return (
+      <BaseContainer>
+        <PostList posts={posts} />
+        <Box ref={fetchTriggerRef} />
+      </BaseContainer>
+    );
+  }
+
   return (
     <BaseContainer>
-      <PostList posts={dummyPosts} />
+      <LoadingMessage />
     </BaseContainer>
   );
 }
