1e2ceaf1c079b4749fb256415ab82e67324ec5d6
jmkim <rookie2000g@gmail.com>
2023-03-03 14:34:28 +0900

    Rewrite npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,8 @@ const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin"
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 const isDevelopment = process.env.NODE_ENV !== "production";
-const useAnalyzer = process.env.ANALYZE === "analyze";
 
 module.exports = {
   context: __dirname,
@@ -69,11 +67,6 @@ module.exports = {
     }),
     isDevelopment && new ReactRefreshWebpackPlugin(),
     !isDevelopment && new MiniCssExtractPlugin(),
-    !isDevelopment &&
-      useAnalyzer &&
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static",
-      }),
   ].filter(Boolean),
   optimization: {
     minimizer: ["...", new CssMinimizerPlugin()],
